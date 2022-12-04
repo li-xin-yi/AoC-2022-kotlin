83fe067b6e6da877f05df9cd4cb23a6a3213d0fe
Xinyi Li <wolixinyi@gmail.com>
2022-12-03 19:43:06 -0500

    day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,40 @@
+import java.util.PriorityQueue
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun parseInput(input: List<String>): List<Int> {
+        val res = mutableListOf<Int>()
+        var cur = 0
+        for (line in input) {
+            if (line == "") {
+                res.add(cur)
+                cur = 0
+            } else {
+                cur += line.toInt()
+            }
+        }
+        res.add(cur)
+        return res
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun solve_part1(input: List<String>): Int {
+        val lst = parseInput(input)
+        return lst.max()
     }
+
+    fun solve_part2(input: List<String>): Int {
+        val lst = parseInput(input).map { it -> -it }
+        val pq = PriorityQueue<Int>(lst)
+        var res = 0
+        for (i in 0 until 3) {
+            res += pq.poll()
+        }
+        return -res
+    }
+
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("input/Day01_test")
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(solve_part1(testInput))
+    println(solve_part2(testInput))
 }
